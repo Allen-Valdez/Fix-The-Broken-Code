934de056703cd895cc59d005e7596bf661f935e5
Allen-Valdez <68507863+Allen-Valdez@users.noreply.github.com>
2020-12-04 21:32:24 -0800

    Code Challenge

---
+++ BrokenCode.js
@@ -0,0 +1,41 @@
+const rollTheDice = () => {
+  // Math.random() gives us a random number from 0 up to, but not including, 1
+  // We multiplied that by 6 to get a number between 0 and up to, but not including, 6
+  // But since we actually wanted numbers from 1 to 6, inclusive, we added 1
+    let die1 = Math.random() * 6 + 1
+    let die2 = Math.random() * 6 + 1
+    return die1 + die2
+}
+
+
+
+
+//Need a hint? Scroll to line 40!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Don't forget to console log what each die is doing.
+//Also don't forget to call upon the function.
